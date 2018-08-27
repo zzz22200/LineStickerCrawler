d16131f30f4376c951a0dad666d7d622951f4f21
Lance <lance@ximple.com.tw>
2018-08-27 16:00:38 +0800

    use BeautifulSoup to get Title

--- crawler.py
+++ crawler.py
@@ -1,31 +1,30 @@
 import urllib.request
 import re
 from urllib.request import urlretrieve
-
-
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 #下載儲存位置
 fileLocation='C:\\Users\\ximple\\Desktop\\line爬圖'
-#設置關鍵字
-keywords = "1471879"
-#quote函數進行url編碼(屏蔽特殊的字符)
-key = urllib.request.quote(keywords)
+#設置要爬的頁面
+url = "https://store.line.me/stickershop/product/4034526/zh-Hant"
+
+
+
 #設置User-Agent
 headers=("User_Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 SE 2.X MetaSr 1.0")
 #自定義opener
 opener = urllib.request.build_opener()
 opener.addheaders = [headers]
 urllib.request.install_opener(opener)
-#循環遍歷抓取
-def getTitle(content):
-    chineseRule = '[\u4e00-\u9fff]+'
-    titleRule = '"mdCMN08Ttl">' + chineseRule
-    titleString = (re.compile(titleRule).findall(content)).__str__()
-    title = re.compile(chineseRule).findall(titleString)
 
+
+
+def getTitle(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    title=soup.find('h3','mdCMN08Ttl').text
     return title
 
 for i in range(0,1):
-    url = "https://store.line.me/stickershop/product/"+key+"/zh-Hant"
     #print(url)
     content = urllib.request.urlopen(url).read().decode("utf-8","ignore")
     rule = '(https://stickershop.line-scdn.net/stickershop/v1/sticker/[0-9]+/ANDROID/sticker.png)' #正則匹配
@@ -36,8 +35,8 @@ for i in range(0,1):
     imglist = re.compile(rule).findall(content) #獲取圖片列表
     for j in range(0,len(imglist)):
         imgurl = imglist[j]
-        file = "\\"+"KUSOHAMU\\"+str(j+1)+".jpg"
+        file = fileLocation+"\\"+title+"\\"+str(j+1)+".jpg"
         urlretrieve(imgurl, filename=file)
         print('第', j +1, '張下載完成!')
-
+print("已全部下載完成")
 
